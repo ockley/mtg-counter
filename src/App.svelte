06effe0a2fce1a326a0c603f6c43eca218ae84a5
ockley <vestergaard.karsten@gmail.com>
2023-09-02 10:54:38 +0200

    New Header

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
   $: redWon = blueScore === 0;
   $: blueWon = redScore === 0;
   $: gameOver = redWon || blueWon;
-  
+
   function newGame() {
     redScore = 20;
     blueScore = 20;
@@ -16,38 +16,34 @@
 
 <div class="row mt-2">
   <div class="col">
-    <h1 class="text-center">MTG Counter</h1>
+    <h1 class="text-center">MTG Counter App</h1>
   </div>
 </div>
 
 <div class="row">
   <Team {gameOver} team="Red" bind:score={redScore} />
-  <Team {gameOver} team="Blue" bind:score={blueScore}/>
+  <Team {gameOver} team="Blue" bind:score={blueScore} />
 </div>
 
 {#if !gameOver}
-<div class="row">
-  <div class="col">
-    <button class="btn btn-warning mt-3 w-100" on:click={newGame}><h2>Reset Game</h2></button>
+  <div class="row">
+    <div class="col">
+      <button class="btn btn-warning mt-3 w-100" on:click={newGame}><h2>Reset Game</h2></button>
+    </div>
   </div>
-</div>
 {:else}
-<div class="rov mt-3">
-  <div class="col">
-    {#if blueWon}
-      <h2 class="text-center text-primary">Blue Won!</h2>
+  <div class="rov mt-3">
+    <div class="col">
+      {#if blueWon}
+        <h2 class="text-center text-primary">Blue Won!</h2>
       {:else}
-      <h2 class="text-center text-danger">Red Won!</h2>
-    {/if}
-    
+        <h2 class="text-center text-danger">Red Won!</h2>
+      {/if}
+    </div>
   </div>
-</div>
-<div class="row">
-  <div class="col">
-    <button class="btn btn-success mt-3 w-100" on:click={newGame}><h2>New Game</h2></button>
-
+  <div class="row">
+    <div class="col">
+      <button class="btn btn-success mt-3 w-100" on:click={newGame}><h2>New Game</h2></button>
+    </div>
   </div>
-</div>
 {/if}
-
-
